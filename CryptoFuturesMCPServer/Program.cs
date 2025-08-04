c2f0f342aaf1e4d347f2cd48fc03204c42ec6b15
ShaoHans <shaomenghao@hotmail.com>
2025-08-04 21:25:17 +0800

    add OKXTools

--- CryptoFuturesMCPServer/Program.cs
+++ CryptoFuturesMCPServer/Program.cs
@@ -1,16 +1,17 @@
+using CryptoFuturesMCPServer.Tools;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OKX.Api;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 // Configure all logs to go to stderr (stdout is used for the MCP protocol messages).
 builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Trace);
 
+builder.Services.AddSingleton<OkxRestApiClient>();
+
 // Add the MCP services: the transport to use (stdio) and the tools to register.
-builder.Services
-    .AddMcpServer()
-    .WithStdioServerTransport()
-    .WithTools<RandomNumberTools>();
+builder.Services.AddMcpServer().WithStdioServerTransport().WithTools<OKXTools>();
 
 await builder.Build().RunAsync();
