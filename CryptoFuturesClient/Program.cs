7423c20f75fc7d2ade7f400818a64e9ebbb70ff5
ShaoHans <shaomenghao@hotmail.com>
2025-08-04 21:45:57 +0800

    create client project test

---
+++ CryptoFuturesClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+var projectRoot = Directory
+    .GetParent(AppContext.BaseDirectory)
+    ?.Parent?.Parent?.Parent?.Parent?.FullName;
+
+var toolsPath = Path.Combine(
+    projectRoot!,
+    "CryptoFuturesMCPServer",
+    "bin",
+    "Debug",
+    "net9.0",
+    "CryptoFuturesMCPServer.dll"
+);
+
+var clientTransport = new StdioClientTransport(
+    new StdioClientTransportOptions { Command = "dotnet", Arguments = new[] { toolsPath }, }
+);
+
+var client = await McpClientFactory.CreateAsync(clientTransport);
+foreach (var tool in await client.ListToolsAsync())
+{
+    Console.WriteLine($"{tool.Name} ({tool.Description})");
+}
+
+var result = await client.CallToolAsync(
+    "get_mark_price",
+    new Dictionary<string, object?>() { ["symbol"] = "BTC-USDT" },
+    cancellationToken: CancellationToken.None);
+
+Console.WriteLine(((TextContentBlock)result.Content.First()).Text);
