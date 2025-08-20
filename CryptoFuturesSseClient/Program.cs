52ff8c682207e5404201e3d8b984b114b37b5b8c
Hans <shaomenghao@hotmail.com>
2025-08-20 16:18:50 +0800

    add CryptoFuturesSseClient project

---
+++ CryptoFuturesSseClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+var transport = new SseClientTransport(
+    new SseClientTransportOptions
+    {
+        Endpoint = new Uri("http://localhost:10900/sse"),
+        Name = "dotnet console client",
+        TransportMode = HttpTransportMode.AutoDetect,
+    }
+);
+
+var client = await McpClientFactory.CreateAsync(transport);
+foreach (var tool in await client.ListToolsAsync())
+{
+    Console.WriteLine($"{tool.Name} ({tool.Description})");
+}
+
+var result = await client.CallToolAsync(
+    "get_futures_real_time_handicap",
+    new Dictionary<string, object?> { { "symbol", "BTCUSDT" } }
+);
+
+Console.WriteLine(((TextContentBlock)result.Content.First()).Text);
